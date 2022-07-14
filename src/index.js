38f4ea38e0b683b0b6288d864bd84f6a72c3ec2f
Trent Freeman <trentfreeman@microsoft.com>
2022-07-14 13:08:11 -0400

    added title and link to post

--- src/index.js
+++ src/index.js
@@ -1,61 +1,88 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as msal from "@azure/msal-browser";
 
-import { Image } from 'react-bootstrap';
+import { Image } from "react-bootstrap";
 
-import './index.css';
+import "./index.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageURL: null,
+      title: "Unknown",
+      link: "",
+    };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            imageURL: null
-        };
+    this.fetchPost = this.fetchPost.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchPost();
+    console.log(this.state.imageURL);
+    this.login();
+  }
+
+  async login() {
+    const msalConfig = {
+      auth: {
+        clientId: "cd9a9a8e-dc07-4180-9ecb-122c5549a5e4",
+      },
+    };
+
+    const msalInstance = new msal.PublicClientApplication(msalConfig);
+    try {
+      const loginResponse = await msalInstance.acquireTokenSilent();
+      console.log(loginResponse);
+    } catch (err) {
+      console.log(err);
     }
 
-    componentDidMount() {
-        this.fetchImage();
-        console.log(this.state.imageURL);
-    }
+    console.log("logged in");
+    const myAccounts = msalInstance.getAllAccounts();
+    console.log(msalInstance.getAccountByUsername("jtrentfreeman@gmail.com"));
+    console.log(myAccounts);
+  }
 
-    fetchImage = (subreddit) => {
-
-        let imageURL;
-
-        fetch('https://old.reddit.com/r/dalle2/random/.json', {
-            method: 'GET',
-            cache: 'no-cache'
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            let media_metadata  = data[0].data.children[0].data.url;
-            // let ids = Object.keys(media_metadata);
-            let url = `https://i.redd.it/${MediaMetadata}.png`;
-            imageURL = media_metadata;
-            this.setState({imageURL: media_metadata});
+  fetchPost = (subreddit) => {
+    fetch("https://old.reddit.com/r/dalle2/random/.json", {
+      method: "GET",
+      cache: "no-cache",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        let media_metadata = data[0].data.children[0].data.url;
+        let title = data[0].data.children[0].data.title;
+        let link =
+          "https://reddit.com/" + data[0].data.children[0].data.permalink;
+        this.setState({
+          imageURL: media_metadata,
+          title,
+          link,
         });
-    }
+      });
+  };
 
-    displayImage = () => {
-        return (
-            <React.Fragment>
-                <h2>This is up next</h2>
-                <Image className='mainImage' src={this.state.imageURL}/>
-                <h3> image should be above</h3>
-            </React.Fragment>
-        )
-    }
+  displayImage = () => {
+    return (
+      <React.Fragment>
+        <h2>{this.state.title}</h2>
+        <Image className="mainImage" src={this.state.imageURL} />
+        <a href={this.state.link}>Link to post</a>
+      </React.Fragment>
+    );
+  };
 
-    render() {
-        return (
-            <div className="App">
-                <h1> Hello, reactttt</h1>
-                {this.displayImage()}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+        <h1> Hello, reactttt</h1>
+        {this.displayImage()}
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
